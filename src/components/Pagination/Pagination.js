4f4aa37129942259235697d4a506bc9c5ddc5c7d
Okikiola Omotosho <omotoshookiki@gmail.com>
2021-01-25 23:30:05 +0100

    prev-next fixed

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,34 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import './Pagination.css'
+import React, { useState, useEffect } from "react";
+import "./Pagination.css";
 
-function Pagination({details, showFunction, monitor}) {
-    const [currentPage, setCurrentPage] = useState(1)
-    const totalProfile = details.length
-    const perPage = 20
-    const numberOfPaginations = Math.ceil(totalProfile/perPage)
-    const startingPoint = (currentPage - 1) * perPage;
-    const shownDetails = details.slice(startingPoint, startingPoint + perPage);
+function Pagination({ details, showFunction, monitor }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalProfile = details.length;
+  const perPage = 20;
+  const numberOfPaginations = Math.ceil(totalProfile / perPage);
+  const startingPoint = (currentPage - 1) * perPage;
+  const shownDetails = details.slice(startingPoint, startingPoint + perPage);
 
+  useEffect(() => {
+    showFunction(shownDetails);
+    // console.log(monitor)
+  }, [currentPage, monitor]);
 
-    useEffect(() => {
-        showFunction(shownDetails)
-        // console.log(monitor)
-    },[currentPage,monitor])
+  const setCurrent = (index) => {
+    setCurrentPage(index + 1);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
-    const setCurrent = (index) => {
-        setCurrentPage(index+1)
-        window.scrollTo({top: 0, left:0, behavior:'smooth'})
-    }
+  const nextFunc = () => {
+    setCurrentPage(prevState => prevState + 1)
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
-    return (
-        <div className='pagination'>
-            {Array(numberOfPaginations).fill(1).map((item, i) => {
-                return(
-                    <div className={`each__btn ${currentPage === i+1 ? 'pagination__bg': ''}`}onClick={() => setCurrent(i)} key={i}>{i+1}</div>
-                )
+  const prevFunc = () => {
+    setCurrentPage(prevState => prevState - 1)
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+
+  };
+  return (
+    <div className="pagination__container">
+        
+      <button className="prev__btn" onClick={currentPage >= 1 ? prevFunc : ''}>Previous</button>
+
+        <div className="pagination">
+            {Array(numberOfPaginations)
+            .fill(1)
+            .map((item, i) => {
+                if (i < 10) {
+                return (
+                    
+                    <div
+                    className={`each__btn ${
+                        currentPage === i + 1 ? "pagination__bg" : ""
+                    }`}
+                    onClick={() =>setCurrent(i)}
+                    key={i}
+                    >
+                    {i + 1}
+                    </div>
+                );
+                }
             })}
         </div>
-    )
+        <button className="next__btn" onClick={currentPage < numberOfPaginations ? nextFunc : ''}>Next</button>
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
